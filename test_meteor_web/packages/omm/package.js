6e0396f581251d207dcd98368b5a39e952d91094
Bert van Heukelom <bert@vanheukelom.de>
2015-07-19 11:45:36 +0200

    Changing to github

--- test_meteor_web/packages/omm/package.js
+++ test_meteor_web/packages/omm/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Object meteor mapper. Maps between rich objects and documents.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://bitbucket.org/bertundmax/meteor-om',
+  git: 'https://github.com/bvanheukelom/omm',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
