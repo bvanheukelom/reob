c1ef23492c2bbb1887d4ae2e4c78bd2aa43aa78f
Bert van Heukelom <bert@vanheukelom.de>
2016-10-11 16:07:40 +0200

    fixed that reop/server works

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+/**
+ * Created by bert on 06.10.16.
+ */
+__export(require("./dist/src/Server"));
